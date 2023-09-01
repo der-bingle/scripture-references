1d94cd7acbea4852b894731fbe8e8b7795118dc4
shadow-light <noreply@gracious.tech>
2023-09-01 12:24:22 +1000

    Client 0.5.3

--- client/src/index.ts
+++ client/src/index.ts
@@ -1,3 +1,4 @@
 
 export {BibleClient} from './client.js'
-export {verses_obj_to_str, verses_str_to_obj, book_name_to_code} from './extras.js'
+export {verses_obj_to_str, verses_str_to_obj, book_name_to_code, passage_obj_to_str,
+    passage_str_to_obj} from './extras.js'
