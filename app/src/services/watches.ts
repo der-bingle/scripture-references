4ab7967be130094f0f10a537774c6ee644c49a30
shadow-light <noreply@gracious.tech>
2024-07-01 18:52:36 +1000

    app: rm unnecessary code

--- app/src/services/watches.ts
+++ app/src/services/watches.ts
@@ -159,8 +159,8 @@ watch(() => state.search, () => {
     const ref = content.collection.detect_passage(state.search ?? '')
     if (ref){
         state.book = ref.book
-        state.chapter = ref.start_chapter ?? 1
-        state.verse = ref.start_verse ?? 1
+        state.chapter = ref.start_chapter
+        state.verse = ref.start_verse
         state.target = [state.chapter, state.verse]
     }
 })
