8d307d90f1f4ee910e3b232b8673694f83253cc0
shadow-light <noreply@gracious.tech>
2025-06-23 21:26:27 +1000

    app: icons

--- app/src/comp/AppIcon.vue
+++ app/src/comp/AppIcon.vue
@@ -10,12 +10,9 @@ component.icon(:is='icons[name]' :class='{small}')
 
 import type {FunctionalComponent} from 'vue'
 
-import add from '@material-symbols/svg-400/outlined/add.svg?component'
-import add_circle from '@material-symbols/svg-400/outlined/add_circle.svg?component'
-import delete_ from '@material-symbols/svg-400/outlined/delete.svg?component'
+import add_circle_fill from '@material-symbols/svg-400/outlined/add_circle-fill.svg?component'
 import close from '@material-symbols/svg-400/outlined/close.svg?component'
 import close_small from '@material-symbols/svg-400/outlined/close_small.svg?component'
-import check from '@material-symbols/svg-400/outlined/check.svg?component'
 import search from '@material-symbols/svg-400/outlined/search.svg?component'
 import more_vert from '@material-symbols/svg-400/outlined/more_vert.svg?component'
 import arrow_back from '@material-symbols/svg-400/outlined/arrow_back.svg?component'
@@ -36,8 +33,8 @@ defineProps({
 
 
 const icons = {
-    close, search, more_vert, add, delete: delete_, arrow_back, add_circle, expand_circle_down,
-    settings, info, dark_mode, light_mode, cloud_off, sync, close_small, check, arrow_outward,
+    close, search, more_vert, arrow_back, add_circle_fill, expand_circle_down,
+    settings, info, dark_mode, light_mode, cloud_off, sync, close_small, arrow_outward,
 } as Record<string, FunctionalComponent>
 
 
