0f92c0797beff7df6547d3654a4d93dc7b6bad66
shadow-light <noreply@gracious.tech>
2025-06-25 12:12:32 +1000

    app: search components

---
+++ app/src/comp/nav/SearchResultsItem.vue
@@ -0,0 +1,67 @@
+
+<template lang='pug'>
+
+v-card(class='mb-4' density='compact' @click='go_to_ref')
+    v-card-item
+        v-card-title(class='text-subtitle-2 font-weight-bold') {{ ref_title }}
+    v-card-text
+        div.scripture(v-html='result.html')
+
+</template>
+
+
+<script lang='ts' setup>
+
+import {computed} from 'vue'
+
+import {change_book, state} from '@/services/state'
+import {content} from '@/services/content'
+
+import type {SearchResult} from '@gracious.tech/fetch-search'
+
+
+const props = defineProps<{result:SearchResult}>()
+
+
+const ref_title = computed(() => {
+    return content.collection.reference_to_string(props.result.ref, state.trans[0])
+})
+
+
+const go_to_ref = () => {
+    change_book(props.result.ref)
+    state.show_nav = false
+    // Since have made use of these search results, save the query to history
+    const query = state.search.trim()
+    state.search_history = [query, ...state.search_history.filter(q => q !== query)].slice(0, 10)
+}
+
+
+</script>
+
+
+<style lang='sass' scoped>
+
+.scripture
+    line-height: 1.2
+    max-height: 200px  // Limit height of results
+    font-size: 14px
+    @media (min-width: 800px)
+        font-size: 15px
+
+    // Fade out when exceed height by adding gradient to bottom edge
+    &::after
+        content: ""
+        position: absolute
+        bottom: 0
+        left: 0
+        right: 0
+        height: 1.5rem
+        background: linear-gradient(to bottom, transparent, rgb(var(--v-theme-surface)))
+
+    :deep(mark)
+        color: inherit
+        background-color: rgb(var(--v-theme-primary), 0.5)
+
+
+</style>
---
+++ app/src/comp/nav/SearchToolbar.vue
@@ -0,0 +1,69 @@
+
+<template lang='pug'>
+
+v-toolbar(:density='density')
+
+    //- autocomplete=off tells browser not to show its own suggestions which would overlap own
+    v-combobox.input(v-model='state.search' hide-details autocomplete='off'
+            density='compact' variant='outlined' autofocus :items='state.search_history')
+        template(#prepend-inner)
+            app-icon.placeholder(v-if='!state.search' name='search')
+        template(#append-inner)
+            v-btn(v-if='state.search' @click='state.search = ""' icon size='small')
+                app-icon(name='close' small)
+
+    v-btn-toggle(v-model='search_filter' color='primary' density='compact'
+            :class='{"mr-2": state.wide}')
+        v-btn(size='x-small' value='ot') Old
+        v-btn(size='x-small' value='nt') New
+        v-btn(size='x-small' value='book') {{ current_book_abbrev }}
+    v-btn(v-if='!state.wide' icon variant='text' @click='state.show_nav = false')
+        app-icon(name='close')
+
+</template>
+
+
+<script lang='ts' setup>
+
+import {computed} from 'vue'
+
+import {density, state} from '@/services/state'
+import {current_book_abbrev} from '@/services/computes'
+
+
+// Proxy so save null rather than undefined
+const search_filter = computed({
+    get(){
+        return state.search_filter
+    },
+    set(value:'ot'|'nt'|'book'|undefined){
+        state.search_filter = value ?? null
+    },
+})
+
+
+</script>
+
+
+<style lang='sass' scoped>
+
+.input
+    margin: 0 8px
+
+    :deep(.v-field)
+        padding-right: 0 !important
+
+    :deep(.v-field__prepend-inner)  // Container of search icon placeholder
+        pointer-events: none
+
+    :deep(.v-combobox__menu-icon)
+        display: none  // Hide Vuetify's dropdown button that comes with combobox
+
+.placeholder
+    opacity: 0.5
+
+.v-toolbar__content > .v-btn:last-child
+    margin-inline-end: 0  // Override Vuetify
+
+
+</style>
