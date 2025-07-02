bfe3372ad9e70f5990adfd33c9de66748b2eab29
shadow-light <noreply@gracious.tech>
2025-07-02 10:40:55 +1000

    client: original word for gloss

--- client/src/glosses.ts
+++ client/src/glosses.ts
@@ -1,6 +1,13 @@
 
+import {rm_diacritics} from './utils.js'
+
 import type {PassageReference} from '@gracious.tech/bible-references'
 import type {GlossesData, GlossesDataWord} from './shared_types'
+
+
+interface GlossesWord extends GlossesDataWord {
+    original:string
+}
 
 
 export class GlossesBook {
@@ -12,9 +19,9 @@ export class GlossesBook {
     }
 
     // Get words with glosses for given verse of book
-    get_words(reference:PassageReference):GlossesDataWord[]
-    get_words(chapter:number, verse:number):GlossesDataWord[]
-    get_words(ch_or_ref:number|PassageReference, verse=1):GlossesDataWord[]{
+    get_words(reference:PassageReference):GlossesWord[]
+    get_words(chapter:number, verse:number):GlossesWord[]
+    get_words(ch_or_ref:number|PassageReference, verse=1):GlossesWord[]{
 
         // Get chapter/verse from ref if given
         if (typeof ch_or_ref !== 'number'){
@@ -22,6 +29,11 @@ export class GlossesBook {
             ch_or_ref = ch_or_ref.start_chapter
         }
         // NOTE Clones objects before returning
-        return (this._data.contents[ch_or_ref]?.[verse] ?? []).map(w => ({...w}))
+        return (this._data.contents[ch_or_ref]?.[verse] ?? [])
+            .map(w => ({
+                ...w,
+                // NOTE Hebrew is unicase, but harmless...
+                original: rm_diacritics(w.word).toUpperCase(),
+            }))
     }
 }
